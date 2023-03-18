661a0a03c38667f0ca3ad42e703c0be96bfb77f5
Dinh Chau <dinhchau001@gmail.com>
2023-03-17 18:24:55 -0700

    Made a new simple OOP

---
+++ basicOOP/BusOOP.py
@@ -0,0 +1,32 @@
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+
+class SpaceShip:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(Vehicle):
+    pass
+
+class Camry(Vehicle):
+    pass
+
+class Enterprise(SpaceShip):
+    pass
+
+School_bus = Bus("School Volvo", 12, 50)
+
+camryxle = Camry("Camry XLE", 28000, 4)
+
+trek = Enterprise("Enterprise", 1000000000, 250)
+
+
+#print(isinstance(School_bus, Vehicle)) <--- use of the isintance with School_bus and Vehicle returns True. 
+
+# print(isinstance(trek, Vehicle)) <----- returns false, trek is made with Class SpaceShip not Vehicle
