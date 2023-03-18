d3584d866e0ce8dcf50548121e7b3e39770f60ec
Dinh Chau <dinhchau001@gmail.com>
2023-03-17 18:34:58 -0700

    Added more to code

--- basicOOP/BusOOP.py
+++ basicOOP/BusOOP.py
@@ -3,14 +3,17 @@ class Vehicle:
         self.name = name
         self.mileage = mileage
         self.capacity = capacity
-
+    def __str__(self):
+        return self.name
+    
 
 class SpaceShip:
     def __init__(self, name, mileage, capacity):
         self.name = name
         self.mileage = mileage
         self.capacity = capacity
-
+    def __str__(self):
+        return self.name
 class Bus(Vehicle):
     pass
 
@@ -30,3 +33,5 @@ trek = Enterprise("Enterprise", 1000000000, 250)
 #print(isinstance(School_bus, Vehicle)) <--- use of the isintance with School_bus and Vehicle returns True. 
 
 # print(isinstance(trek, Vehicle)) <----- returns false, trek is made with Class SpaceShip not Vehicle
+
+print(trek)
